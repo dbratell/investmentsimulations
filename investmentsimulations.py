142410acce85d1f53e8da912c10aca1d73fd8d09
Daniel Bratell <bratell@lysator.liu.se>
2015-06-07 00:05:36 +0200

    Experimenting with a monte carlo simulator for stocks.

---
+++ investmentsimulations.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+
+STOCK = (
+    # Name, count, price, dividend
+    ("Investor", 100, 200, 7),
+    ("Telia", 100, 50, 2.1),
+    )
+    
+def main():
+    print("hej")
+
+if __name__ == "__main__":
+    main()
+	
